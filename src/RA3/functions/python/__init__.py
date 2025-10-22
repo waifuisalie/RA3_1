0ac122f6130a5030ac8cd926410851b74c3ca644
Seixas, Stefan <stefanme48@gmail.com>
2025-10-22 00:58:40 -0300

    attributes and symbol table implemented. Also, unit tests for symbol table

---
+++ src/RA3/functions/python/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Integrantes do grupo (ordem alfabética):
+# Breno Rossi Duarte - breno-rossi
+# Francisco Bley Ruthes - fbleyruthes
+# Rafael Olivare Piveta - RafaPiveta
+# Stefan Benjamim Seixas Lourenco Rodrigues - waifuisalie
+#
+# Nome do grupo no Canvas: RA3_1
+
+"""
+RA3 - Fase 3: Analisador Semântico
+
+Este pacote contém os módulos para análise semântica da linguagem RPN:
+- tipos.py: Sistema de tipos e promoção de tipos
+- tabela_simbolos.py: Gerenciamento da tabela de símbolos
+- gramatica_atributos.py: Definição da gramática de atributos e regras semânticas
+"""
+
+__all__ = ['tipos', 'tabela_simbolos', 'gramatica_atributos']
